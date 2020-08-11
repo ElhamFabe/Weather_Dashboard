650e1c72cc92761a1b02d57f9c7a540e5882ffa1
Elham Fabe <elhamfabe@gmail.com>
2020-08-10 21:49:14 -0400

    ajax x2, html

--- Assets/weatherDashboard.js
+++ Assets/weatherDashboard.js
@@ -1,37 +1,92 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    // create local storage from search inout
 
-$('#weathersubmit').on("click",function (event){
-    event.preventDefault();
+    // create weather description results
+    //get items from local stoarge
 
-console.log("click");
-// Empty search results
-clear();
+    // create updating current location forecast
 
-var city = $("#city").val();
+    // create on click function 
+    $('#weathersubmit').on("click", function (event) {
+        event.preventDefault();
 
-// adding url from opn weather app, city of user choice, units=imperial (farenheit) and api key
-var queryUrl = "https://www.api.openweathermap.org/data/2.5/forecast?q=" + city + " units=imperial" + "&appid=048259ccf3f84222f1781be5f3d4ba8e";
+        console.log("click");
+        // Empty search results
+        $('#clear-all').empty();
 
-if (!city === '') {
-    $.ajax ({
+        // apppend search results to weather description container
+        // $('#city').append(response);
+
+        var city = $("#city").val();
+
+        // adding url from opn weather app, city of user choice, units=imperial (farenheit) and api key
+        if (city !== '') {
+            //check if city is already in LS
+            
+       //save city to local storage
+            getWeather(city)
+        } else {
+            $("error").html('Can not leave city blank' + 'Try again')
+        }
+
+
+    });
+
+
+function getWeather (city){
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=048259ccf3f84222f1781be5f3d4ba8e";
+    console.log(city)
+    $.ajax({
         url: queryUrl,
-        method: "GET",
-        datatype: "jsonp"
+        method: "GET"
+        // datatype: "jsonp"
 
-    }).then(function(response){
-    var 
+    }).then(function (response) {
+        // lat & long
+        var lat = response.city.coord.lat
+        var lon = response.city.coord.lon
 
-    }),
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&units=imperial&appid=048259ccf3f84222f1781be5f3d4ba8e`
 
-}else {
-    $("error").html('Can not leave city blank' + 'Try again')
+        $.ajax({
+            url: weatherUrl,
+            method: "GET"
+
+        }).then(function (data) {
+            console.log(data);
+            var weatherDiv = $('#weatherDescription');
+            var date = moment.unix(data.daily[0].dt).format('MMMM Do YYYY');
+            // html containing weather description from search
+            $('#date').text(date)
+            $('#description').text(data.daily[0].weather[0].description);
+            $('#tempMin').text(data.daily[0].temp.min);
+            $('#tempMax').text(data.daily[0].temp.max);
+            $('#humidity').text(data.daily[0].humidity);
+            $('#uvIndex').text(data.daily[0].uvi);
+
+            var forecastDiv = $('#forecast');
+            for (var i = 1; i < 6; i++) {
+                var htmlStr = ` <h1 id="description">${data.daily[i].weather[0].description}</h1>
+                <p id="tempMin">${data.daily[i].temp.min}</p>
+                <p id="tempMax"></p>
+                <p id="humidity"></p>
+                <p id="uvIndex"></p>
+                <p id="windSpeed"></p>`
+
+                forecastDiv.append(htmlStr);
+
+            }
+
+        })
+
+
+
+
+
+
+    });
+
 }
-
-
-});
-
-
-
 
 
 
